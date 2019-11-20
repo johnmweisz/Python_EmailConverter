1e6b65101119ebf966515b53382fdd0777df7d0f
John Weisz <johnmweisz@gmail.com>
2019-11-19 19:47:30 -0800

    Converts mbox file to csv email list

---
+++ EmailConverter.py
@@ -0,0 +1,25 @@
+import mailbox
+import csv
+from email.header import decode_header, make_header
+
+writer = csv.writer(open("Converted.csv", "w"))
+emails = {}
+
+for message in mailbox.mbox('mail.mbox'):
+    if not message['from'] in emails:
+        try:
+            make_header(decode_header(message['subject']))
+        except:
+            Subject = message['subject']
+        else:
+            Subject = make_header(decode_header(message['subject']))
+        try:
+            make_header(decode_header(message['from']))
+        except:
+            From = message['from']
+        else:
+            From = make_header(decode_header(message['from']))
+        
+        writer.writerow([ message['date'], From, Subject ])
+
+        emails[str(From)] = 1
